67d8d70d9145422b35151706c5a1d5c1751658dd
Daniel Salvagni <danielsalvagni@gmail.com>
2019-02-03 20:02:17 +0100

    fixes styles mapping on StatusMessage

--- src/constants/StatusMessage.js
+++ src/constants/StatusMessage.js
@@ -5,24 +5,24 @@ import Icon from "../components/Icon/Icon";
 import style from "../components/Message/Message.scss"
 
 const StatusMessage = {
-  [Status.EMPTY]: <p>Drop your photo here or tap to select.</p>,
+  [Status.EMPTY]: <p className={style["message__text"]}>Drop your photo here or tap to select.</p>,
   [Status.LOADING]:<Icon name="loading" size={48}/>,
   [Status.DRAGOVER]: (
     <Fragment>
       <Icon name="upload" size={48}/>
-      <p className={style["message_text"]}>Drop your photo</p>
+      <p className={style["message__text"]}>Drop your photo</p>
     </Fragment>
   ),
   [Status.INVALID_FILE_TYPE]: (
     <Fragment>
-      <p className={style["message_text"]}>Only images allowed.</p>
-      <p className={style["message_text"]}>Drop your photo here or tap to select.</p>
+      <p className={style["message__text"]}>Only images allowed.</p>
+      <p className={style["message__text"]}>Drop your photo here or tap to select.</p>
     </Fragment>
   ),
   [Status.INVALID_IMAGE_SIZE]: (
     <Fragment>
-      <p className={style["message_text"]}>Your photo must be larger than 350px.</p>
-      <p className={style["message_text"]}>Drop your photo here or tap to select.</p>
+      <p className={style["message__text"]}>Your photo must be larger than 350px.</p>
+      <p className={style["message__text"]}>Drop your photo here or tap to select.</p>
     </Fragment>
   ),
   [Status.LOADED]: null
