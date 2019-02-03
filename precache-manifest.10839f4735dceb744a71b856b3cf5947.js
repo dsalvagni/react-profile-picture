c728e20322287fa0a414e08822cf32b353a7c156
Daniel Salvagni <danielsalvagni@gmail.com>
2019-02-03 19:58:14 +0100

    Updates

---
+++ precache-manifest.10839f4735dceb744a71b856b3cf5947.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b7b33764f466c9d25bd",
+    "url": "/react-profile-picture/static/css/main.3a90364f.chunk.css"
+  },
+  {
+    "revision": "8b7b33764f466c9d25bd",
+    "url": "/react-profile-picture/static/js/main.8b7b3376.chunk.js"
+  },
+  {
+    "revision": "250c90d4488815004b50",
+    "url": "/react-profile-picture/static/css/1.9e1d6e03.chunk.css"
+  },
+  {
+    "revision": "250c90d4488815004b50",
+    "url": "/react-profile-picture/static/js/1.250c90d4.chunk.js"
+  },
+  {
+    "revision": "3127d416680c07979631",
+    "url": "/react-profile-picture/static/js/runtime~main.3127d416.js"
+  },
+  {
+    "revision": "aea3bfc16d47eb5d98b7dd73af027a04",
+    "url": "/react-profile-picture/static/media/example.aea3bfc1.jpg"
+  },
+  {
+    "revision": "f4d5c428f66adec303ab76338193b5ec",
+    "url": "/react-profile-picture/index.html"
+  }
+];
