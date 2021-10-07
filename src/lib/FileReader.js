c95b5a1a1eef2f1ec652c5bc0338bf9505d83193
Daniel Salvagni <daniel.salvagni@gmail.com>
2021-10-07 10:27:58 +0200

    fixes #30 #24

--- src/lib/FileReader.js
+++ src/lib/FileReader.js
@@ -4,25 +4,27 @@ const UNKNOWN = "UNKNOWN";
 /**
  * Wrapper for the JS FileReader API.
  * @author Daniel Salvagni <danielsalvagni@gmail.com>
- * 
+ *
  * @param {File} file Image file
  * @param {object} settings Function's settings
- * @param {function} settings.onError Error Callback 
+ * @param {function} settings.onError Error Callback
  * @param {function} settings.onLoadStart Start Callback
  * @param {function} settings.onLoadEnd Finished loading callback
  * @returns {object} FileReader instance
  */
 const fileReader = (file, settings) => {
-  if (!file.type.match("image.*"))
-    if (typeof config.onError === "function")
-      config.onError.call(this, { error: INVALID_FILE_TYPE });
-
   const config = {
     onError: () => {},
     onLoadStart: () => {},
     onLoadEnd: () => {},
-    ...settings
+    ...settings,
   };
+
+  if (!file.type.match("image.*")) {
+    if (typeof config.onError === "function")
+      config.onError.call(this, { error: INVALID_FILE_TYPE });
+    return;
+  }
 
   const reader = new FileReader();
 
@@ -30,11 +32,11 @@ const fileReader = (file, settings) => {
     if (typeof config.onLoadStart === "function")
       config.onLoadStart.call(this, { file });
   };
-  reader.onloadend = data => {
+  reader.onloadend = (data) => {
     if (typeof config.onLoadEnd === "function")
       config.onLoadEnd.call(this, {
         base64Image: data.target.result,
-        type: file.type
+        type: file.type,
       });
   };
   reader.onerror = () => {
